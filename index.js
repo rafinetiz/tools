a7d267c339dd69a4b1774a54b322c5f71f7c41f1
Angger Rafi Akbar <rafii.netizz@gmail.com>
2022-10-10 16:40:26 +0700

    first

---
+++ index.js
@@ -0,0 +1,112 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const axios = require('axios').default;
+
+const http = axios.create({
+    timeout: 5000,
+    insecureHTTPParser: true
+});
+
+function print(message, newline=true) {
+    process.stdout.write(message + newline ? '\n' : '');
+}
+
+async function ping(host) {
+    return new Promise((resolve, reject) => {
+        const ping = spawn('ping', ['-W', '2', '-c', '1', host]);
+
+        ping.on('error', (error) => {
+            reject(error);
+        });
+
+        ping.on('close', (code) => {
+            if (code == 0) {
+                resolve(true);
+            } else {
+                reject('Host is unreachable');
+            }
+        });
+    });
+}
+
+async function scan(ip) {
+    return new Promise((resolve, reject) => {
+        let success  = 0;
+        let fail = 0;
+        let totalhost = 0;
+
+        const masscan_opts = [
+            '--rate', '2000',
+            '-p80',
+            '--interface', 'wlan0',
+            '--router-mac', '3c:78:43:ed:30:e1',
+            `${ip}/16`
+        ];
+        
+        //console.log('Scanning', ip);
+        const path    = 'result/' + ip + '.txt';
+        const result  = fs.createWriteStream(path);
+        const masscan = spawn('masscan', masscan_opts);
+
+        masscan.stdout.on('data', async (data) => {
+            const a  = data.toString().trim();
+            const ip = a.match(/(\d{1,3}\.){3}\d{1,3}/g)[0];
+
+            try {
+                const response = await http.get(`http://${ip}`);
+                const regex = response.data.match(/var modelName="([\w-]+)";/);
+
+                if (regex) {
+                    const title = regex[1];
+
+                    result.write(`${ip} - ${title}\n`);
+                } else {
+                    result.write(`${ip} - ${response.status} - ${response.statusText}\n`);
+                }
+
+                success++;
+            } catch (error) {
+                console.log(`  ${ip} - ${error.message}`);
+                fail++;
+            }
+        });
+
+        masscan.on('error', (error) => {
+            reject(error);
+        });
+
+        masscan.on('close', (code) => {
+            result.close();
+
+            if (success == 0) {
+                fs.rmSync(path);
+            }
+
+            resolve([code, success, fail]);
+        });
+    });
+}
+
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
+(async () => {
+    const n = range(9, 254);
+
+    for (const i of n) {
+        const ip = `10.${i}.0`;
+
+        process.stdout.write(`Pinging ${ip}\t`);
+        try {
+            await ping(ip + '.1');
+            console.log('Scanning');
+
+            const [code, success, fail] = await scan(ip + '.0');
+            console.log(`  ${success} success, ${fail} fail`);
+        } catch (error) {
+            console.log(error)
+            continue;
+        }
+    }
+})();
---
+++ main.js
@@ -0,0 +1,44 @@
+const axios = require('axios').default;
+const fs = require('fs');
+
+(async () => {
+    
+    const file = fs.readFileSync(process.argv[2], 'utf8');
+    const a = file.split('\n');
+    
+    for (let i = 0; i < a.length; i++) {
+        const ip = a[i].split(' - ')[0];
+
+        if (!ip) return;
+
+        try {
+            process.stdout.write(`\x1b[Kprocessing ${ip} (${i} of ${a.length})\r`);
+
+            const body = await axios.get(`http://${ip}`, {
+                timeout: 10000,
+                insecureHTTPParser: true
+            });
+            
+            const regex = /(js\/native.js\?1\.1)/;
+
+            if (regex.test(body.data)) {
+                console.log(`\x1b[K\x1b[32m ${ip} SPC FOUND\x1b[0m`)
+            }
+        } catch (err) {
+            // console.log(`  error: ${err.message}`)
+            continue;
+        }
+    }
+    /*
+    try {
+        const body = await axios.get(process.argv[2]);
+        
+        const regex = /(js\/native.js\?1\.1|Getxmllogin\(\))/;
+
+        if (regex.test(body.data)) {
+            console.log('OK');
+        }
+    } catch (error) {
+        console.log(error.message);
+    }*/
+})();
